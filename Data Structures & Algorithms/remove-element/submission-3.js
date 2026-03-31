3489eb9860672a4816b862ad5c8dee07fe23df99
Amit Anand <amitananddotxyz@gmail.com>
2026-03-31 21:28:20 +0530

    Add: remove-element - submission-3

---
+++ Data Structures & Algorithms/remove-element/submission-3.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} val
+     * @return {number}
+     */
+    removeElement(nums, val) {
+        let ptr = 0;
+        for(let i = 0; i < nums.length; i++) {
+            if(nums[i] !== val) {
+                nums[ptr] = nums[i]; 
+                ptr++;
+            }
+        }
+
+        return ptr;
+    }
+}
