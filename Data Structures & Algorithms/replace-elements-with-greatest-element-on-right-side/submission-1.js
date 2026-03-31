d1c4df5809d0d39421bb54648f5ba67b8126fc2b
Amit Anand <amitananddotxyz@gmail.com>
2026-03-31 22:17:06 +0530

    Add: replace-elements-with-greatest-element-on-right-side - submission-1

---
+++ Data Structures & Algorithms/replace-elements-with-greatest-element-on-right-side/submission-1.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[]} arr
+     * @return {number[]}
+     */
+    replaceElements(arr) {
+        // Brute force
+        
+        let i = 0;
+        let n = arr.length;
+
+        while (i < n) {
+            let localizedMax = 0;
+            for(let j = i + 1; j < n; j++) {
+                if(arr[j] > localizedMax) {
+                    localizedMax = arr[j];
+                }
+            }
+
+            arr[i] = localizedMax;
+
+            if(i === n - 1) {
+                arr[i] = -1;
+            }
+
+            i++;
+        }
+
+        return arr;
+    }
+}
